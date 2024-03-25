5edb0eb0328e4d7e0c688f2772cb680411a47fad
Lucas Stasiak <108033145+StasiakLucas@users.noreply.github.com>
2024-03-25 22:44:56 +0000

    Versão atualizada

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const botoes = document.querySelectorAll('.botao');
+const textos = document.querySelectorAll('.aba-conteudo');
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T23:59:59");
+const tempoAtual = new Date();
 
 for(let i=0; i < botoes.length; i++) {
   botoes[i].onclick = function() {
     
     for(let k= 0; k < botoes.length; k++) {
       botoes[k].classList.remove('ativo');
+      textos[k].classList.remove('ativo');
     }
     
     botoes[i].classList.add('ativo');
+    textos[i].classList.add('ativo');
   }
 }
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
